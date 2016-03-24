2cc77dceca033982711dc9e3e9112631276f90a1
Barsha Shrestha <shrestha.barsha17@gmail.com>
2016-03-24 19:21:57 +0000

    Updated seeds.rb with Employee information

--- Timecard/db/seeds.rb
+++ Timecard/db/seeds.rb
@@ -6,18 +6,16 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
-movies = [{:title => 'Aladdin', :rating => 'G', :release_date => '25-Nov-1992'},
-    	  {:title => 'The Terminator', :rating => 'R', :release_date => '26-Oct-1984'},
-    	  {:title => 'When Harry Met Sally', :rating => 'R', :release_date => '21-Jul-1989'},
-      	  {:title => 'The Help', :rating => 'PG-13', :release_date => '10-Aug-2011'},
-      	  {:title => 'Chocolat', :rating => 'PG-13', :release_date => '5-Jan-2001'},
-      	  {:title => 'Amelie', :rating => 'R', :release_date => '25-Apr-2001'},
-      	  {:title => '2001: A Space Odyssey', :rating => 'G', :release_date => '6-Apr-1968'},
-      	  {:title => 'The Incredibles', :rating => 'PG', :release_date => '5-Nov-2004'},
-      	  {:title => 'Raiders of the Lost Ark', :rating => 'PG', :release_date => '12-Jun-1981'},
-      	  {:title => 'Chicken Run', :rating => 'G', :release_date => '21-Jun-2000'},
+employees = [{id => '001', :First_Name => 'Ryan', :Last_Name => 'Smith', :username => 'smithr', :password => '12345', :hours_worked => '8', :Pay_per_hour => '60', :isManager => 0 },
+    	  {id => '002', :First_Name => 'Sheila', :Last_Name => 'Headlee', :username => 'headlees', :password => '12345', :hours_worked => '8', :Pay_per_hour => '60', :isManager => 0 },
+    	  {id => '003', :First_Name => 'Chad', :Last_Name => 'Anderson', :username => 'andersonc', :password => '12345', :hours_worked => '5', :Pay_per_hour => '10', :isManager => 1 },
+      	  {id => '004', :First_Name => 'Sean', :Last_Name => 'Conrad', :username => 'conrads', :password => '12345', :hours_worked => '5', :Pay_per_hour => '60', :isManager => 1 },
+      	  {id => '005', :First_Name => 'Ron', :Last_Name => 'Peterson', :username => 'pertersonr', :password => '12345', :hours_worked => '5', :Pay_per_hour => '60', :isManager => 1 },
+    	  {id => '006', :First_Name => 'Todd', :Last_Name => 'Simpson', :username => 'simpsont', :password => '12345', :hours_worked => '5', :Pay_per_hour => '60', :isManager => 1 },
+    	  {id => '007', :First_Name => 'Jana', :Last_Name => 'Kirchman', :username => 'kirchmanj', :password => '12345', :hours_worked => '5', :Pay_per_hour => '60', :isManager => 1 },
+      	  {id => '008', :First_Name => 'Ryan', :Last_Name => 'Jackson', :username => 'jacksonr', :password => '12345', :hours_worked => '5', :Pay_per_hour => '60', :isManager => 1 }
   	 ]
 
-movies.each do |movie|
-  Movie.create!(movie)
+employees.each do |employee|
+  Employee.create!(employee)
 end
