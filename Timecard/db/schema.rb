52ca65516dc2467f098a938bc35266c9207c6a8f
naloamo <nazirduke@yahoo.com>
2016-03-24 18:49:24 +0000

    employee database added

--- Timecard/db/schema.rb
+++ Timecard/db/schema.rb
@@ -13,13 +13,14 @@
 
 ActiveRecord::Schema.define(version: 20150809022253) do
 
-  create_table "movies", force: :cascade do |t|
-    t.string   "title"
-    t.string   "rating"
-    t.text     "description"
-    t.datetime "release_date"
-    t.datetime "created_at"
-    t.datetime "updated_at"
+  create_table "employees", force: :cascade do |t|
+    t.string   "First_Name"
+    t.string   "Last_Name"
+    t.text     "username"
+    t.datetime "password"
+    t.datetime "hours_worked"
+    t.datetime "Pay_per_hour"
+    t.datetime "isManager"
   end
 
 end
