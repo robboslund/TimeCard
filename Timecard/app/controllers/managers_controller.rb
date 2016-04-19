5e1ca3b86cde23a1e551ad8cb9e35320afd72097
William Kile <montresor35@gmail.com>
2016-04-19 18:07:06 +0000

    added fuctionality to modify stored hours

--- Timecard/app/controllers/managers_controller.rb
+++ Timecard/app/controllers/managers_controller.rb
@@ -1,6 +1,7 @@
 class ManagersController < EmployeesController
   def manager_view_hours
     @employee_hours = Employee_Hour.all
+    @employee =Employee.all
   end
   def change_status
     status = params[:status]
@@ -10,9 +11,18 @@ class ManagersController < EmployeesController
       status ="pending"
     when '1'
       status ="approved"
+      if (Employee_Hour.find_by(id).status != "approved")
+        emp= Employee.find_by username: params[:object][:user]
+        hours= params[:object][:hours].to_i + emp.hours_worked
+       Employee.update(emp.id,:hours_worked => hours)
+     end
     when '2'
       status ="denied"
-      
+      if (Employee_Hour.find_by(id).status == "approved")
+        emp= Employee.find_by username: params[:object][:user]
+        hours=  emp.hours_worked - params[:object][:hours].to_i 
+        Employee.update(emp.id,:hours_worked => hours)
+     end  
     end
     Employee_Hour.update(id, :status => status)
     redirect_to :back
